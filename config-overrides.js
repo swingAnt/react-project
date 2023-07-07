9101cb6113c939c6843fd1c8c840ba2dd32fabc3
lijunkai7 <lijunkai7@jd.com>
2023-07-07 21:11:59 +0800

    fix 打包支持多目录嵌套

--- config-overrides.js
+++ config-overrides.js
@@ -2,16 +2,17 @@
 const path = require('path')
 const { override, fixBabelImports, addWebpackAlias ,addPostcssPlugins} = require('customize-cra')
 const px2viewport = require('postcss-px-to-viewport')
+const CopyPlugin = require("copy-webpack-plugin");
+
+
 
 const babelPlugins = fixBabelImports('import', {
   libraryName: 'antd-mobile',
   style: 'css',
 })
 
-const webpackAlias = addWebpackAlias({
-  '@': path.resolve(__dirname, 'src'),
-  '@scss': path.resolve(__dirname, 'src', 'assets', 'styles'),
-})
+
+
 // 配置 PostCSS 样式转换插件
 const postcssPlugins = addPostcssPlugins([
     // 移动端布局 viewport 适配方案
@@ -22,6 +23,33 @@ const postcssPlugins = addPostcssPlugins([
       // selectorBlackList: ['.ignore', '.hairlines']
     })
   ])
-  
 // 导出要进行覆盖的 webpack 配置
-module.exports = override(babelPlugins, webpackAlias,postcssPlugins)
+module.exports = {'webpack': override(
+  (config) => {
+    config.output = {
+      path: path.join(__dirname, process.env.BUILD||'./build'),
+      publicPath: process.env.PUBLIC_URL||'',
+      }
+      config.resolve.alias={
+        '@': path.resolve(__dirname, 'src'),
+        '@scss': path.resolve(__dirname, 'src', 'assets', 'styles'),
+      }
+        config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+          {
+          from: 'public',
+          to: config.output.path,
+          globOptions: {
+            dot: true,
+            gitignore: true,
+            ignore: ['**.html'],
+          },
+          },
+          ],
+          }),
+        
+        )
+    return config
+  }),babelPlugins,
+  postcssPlugins}
